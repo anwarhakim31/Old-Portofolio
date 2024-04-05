381c4a02cc6eb5301f2d091d926ecc2b76fcdb3e
Anwar Hakim <anwarhakim31@gmail.com>
2024-04-05 14:30:48 +0700

    portofolio slicing figma (DONE)

--- script.js
+++ script.js
@@ -8,4 +8,49 @@ burger.addEventListener("click", function () {
   burger.classList.toggle("active");
 });
 
-const hero = document.querySelector(".hero");
+var typed = new Typed(".auto-type", {
+  strings: ["Front-End Developer"],
+  typeSpeed: 250,
+  backSpeed: 150,
+  loop: true,
+});
+
+const toTop = document.querySelector(".toTop");
+
+window.addEventListener("scroll", function () {
+  const scrollposition = window.scrollY;
+
+  if (scrollposition >= window.innerHeight * 0.01) {
+    toTop.style.display = "block";
+    setTimeout(() => {
+      toTop.style.display = "none";
+    }, 250);
+
+    setTimeout(() => {
+      toTop.style.display = "block";
+    }, 500);
+  } else {
+    setTimeout(() => {
+      toTop.style.display = "none";
+    }, 600);
+    toTop.style.display = "none";
+  }
+});
+
+toTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+let section = document.querySelectorAll("section");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 400;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    }
+  });
+};
